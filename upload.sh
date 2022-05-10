3d5476a22833374a2df406f15a55b6e54719ca31
phdfar <iplserver01@gmail.com>
2022-05-10 07:21:01 +0000

    add img

---
+++ upload.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git add .;  
+git commit -m "add img"
+git config --global user.email 'iplserver01@gmail.com'
+git config --global user.name 'phdfar'
+git push -f https://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@github.com/phdfar/model_segmentation.git
